fcbaf0de06ffcda929a97a6852e37c4ba9c65a0e
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2023-02-23 22:17:09 -0600

    started on api/notes post

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
 const PORT = 3002;
+const databaseJSON = require('./db/db.json');
 const app = express();
 const fs = require('fs');
+const uuid = require('./helpers/uuid');
 // const db = require(./db/db.json)
 
 app.use(express.urlencoded({ extended: true }));
@@ -21,8 +23,29 @@ app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
+app.get('/api/notes', (req, res) => res.json(databaseJSON));
 
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add a review`);
+    
+    const { title, text } = req.body;
+    if (title && text) {
+     
+        const newNote = {
+          title,
+          text,
+          review_id: uuid(),
+        };
+    }
+})
 
+//came from assignment 19
+// fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+//     // err ? console.log(err) : dbArray = JSON.parse(data);
+//     if(err) throw err
+// })
+
+ 
 
 
 

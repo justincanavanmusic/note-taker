f8ad71583d4b3b6347a0d7f44c828b9e90de00eb
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2023-02-24 09:31:15 -0600

    started get readfile

--- server.js
+++ server.js
@@ -18,7 +18,15 @@ app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
-app.get('/api/notes', (req, res) => res.json(databaseJSON));
+// app.get('/api/notes', (req, res) => res.json(databaseJSON));
+
+app.get('/api/notes', (req, res) =>{
+  fs.readFile('db.json', 'utf-8', (err, dbJSONString) => {
+    if (err) {
+      console.error(err)
+    }
+  })
+})
 
 app.post('/api/notes', (req, res) => {
     console.info(`${req.method} request received to add a review`);
